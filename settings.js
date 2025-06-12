1bee5c4911404555da431da1281714ffb1756f78
Zeroxid999 <zeroxid999@gmail.com>
2025-06-12 09:02:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=xntU0IhK#bYcUKMpdzLBuWNgfadIL3Z4zXd2-NJRRJlPZCINMyJo' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=92M2xAwD#Np2FSD8Q3mCAKZfnmSpkvRyOAi_891DPGORwSzzopMY' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
